24c267d61de9b23f3cda76adeae6f782fe0ac2a8
zephyr <zephyr@unity-ecu.stemgrim.net>
2022-05-02 20:33:35 -0400

    Updated readme and adding initial setup.

--- readme.txt
+++ readme.txt
@@ -1,3 +1,16 @@
-Just playing around with the youtube api.
+I've created this utility to help me identify deleted/removed videos on my youtube playlists. 
 
-This setup requires an API Key and your username. 
+This setup requires an API Key and your youtube username in a file named yt_val.py. 
+
+To get an api key: https://developers.google.com/youtube/registering_an_application 
+
+# Example file: 
+vi yt_val.py 
+muser = "youtube_username_here"
+mkey = "api_key_here"
+
+
+The results of the search will appear in a csv named playlist_results.csv. 
+This will allow you to identify video names if any items are removed going forward, while also providing upload dates and video id's for removed items that can be identified on the wayback machine. 
+
+Wayback machine: https://archive.org/web/
--- youtube.py
+++ youtube.py
@@ -11,55 +11,71 @@ class YoutubeApi:
         self.yt_username = yt_username
         self.yt_api_root = "https://youtube.googleapis.com/youtube/v3/"
 
-    def _make_request(self, url, request_type):
+    def _make_api_request(self, url, request_type):
         try:
-            headers = {"Accept": "application/json"}
-            request_result = getattr(requests, request_type)(url, headers=headers)
-            request_result.raise_for_status()
-            request_result_json = request_result.json()
-            request_result_json = request_result_json["items"]
+            api_request = getattr(requests, request_type)(
+                url, headers={"Accept": "application/json"}
+            )
+            api_request.raise_for_status()
+            api_request_json = api_request.json()
+            api_request_json = api_request_json["items"]
         except Exception as query_error:
             print(f"Lookup attempt for url {url} failed with exception {query_error}")
-            request_result_json = {"fail": True}
+            api_request_json = {"fail": True}
 
-        return request_result_json
+        return api_request_json
 
     def _get_my_user(self):
         my_user_url = (
             f"{self.yt_api_root}channels?forUsername={self.yt_username}&"
             + self.my_api_key_url
         )
-        user_details = self._make_request(my_user_url, "get")
-        return user_details[0]["id"]
+        user_details = self._make_api_request(my_user_url, "get")
+        user_id = user_details[0]["id"]
 
-    def _get_my_playlists(self):
-        user_id = self._get_my_user()
+        return user_id
+
+    def _get_users_playlists(self, user_id):
         my_playlist_url = (
             f"{self.yt_api_root}playlists?channelId={user_id}&" + self.my_api_key_url
         )
-        playlist_details = self._make_request(my_playlist_url, "get")
-        playlist_details = playlist_details[0]["id"]
-        return playlist_details
+        playlist_details = self._make_api_request(my_playlist_url, "get")
 
-    def get_playlist_videos(self):
-        playlist_id = self._get_my_playlists()
+        # Use list comprehension to filter out playlist ids from list of dictionaries
+        playlist_id_list = [x["id"] for x in playlist_details if "id" in x]
 
-        my_playlistitem_url = (
-            f"{self.yt_api_root}playlistItems?part=snippet&playlistId={playlist_id}&maxResults=50&"
-            + self.my_api_key_url
-        )
-        playlist_contents = self._make_request(my_playlistitem_url, "get")
-        return playlist_contents
+        return playlist_id_list
+
+    def _get_playlist_videos(self, playlist_id_list):
+        playlist_videos = {}
+        for playlist_id in playlist_id_list:
+            my_playlistitem_url = (
+                f"{self.yt_api_root}playlistItems?part=snippet&playlistId={playlist_id}&maxResults=50&"
+                + self.my_api_key_url
+            )
+            playlist_contents = self._make_api_request(my_playlistitem_url, "get")
+            playlist_videos[playlist_id] = playlist_contents
+
+        return playlist_videos
 
     def write_to_csv(self):
-        playlist_videos = self.get_playlist_videos()
-        with open("test.csv", "w", encoding="utf-8") as result_file:
-            result_file.write("Video_title\n")
-            for video in playlist_videos:
-                result_file.write(str(video["snippet"]["title"] + "\n"))
+        user_id = self._get_my_user()
+        playlist_id_list = self._get_users_playlists(user_id)
+        playlist_videos = self._get_playlist_videos(playlist_id_list)
+
+        with open("playlist_results.csv", "w", encoding="utf-8") as result_file:
+            result_file.write("video_title,video_id,playlist_id,publish_date\n")
+            for playlist_id, video_dict in playlist_videos.items():
+                for yt_video in video_dict:
+                    try:
+                        video_details = f'{str(yt_video["snippet"]["title"]).replace(",", "").lower()},{str(yt_video["snippet"]["resourceId"]["videoId"]).replace(",", "").lower()},{playlist_id},{str(yt_video["snippet"]["publishedAt"]).replace(",", "")}'
+                        result_file.write(video_details + "\n")
+                    except KeyError:
+                        print(f"Error when processing values for item: {video_dict}")
 
 
 if __name__ == "__main__":
     from yt_val import mkey, muser
+
     youtube_instance = YoutubeApi(mkey, muser)
     youtube_instance.write_to_csv()
